534d8f5b6f2ca55f5533e4050d8f58cb99da0360
horatio nelson <admiralofinternetmeme@outlook.com>
2019-01-31 10:21:02 +0700

    soket example

---
+++ socket/udp_client.py
@@ -0,0 +1,20 @@
+import socket
+import time
+
+ADDRESS = "127.0.0.1"
+PORT = 1443
+
+def connect():
+    soket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    while(True):
+        input(strToSend)
+        soket.sendto(strToSend, (ADDRESS, PORT))
+        start = time.time()
+        try:
+            data, server = soket.recvfrom(2048)
+            end = time.time()
+            latency = end - start
+            print(data, " ", latency, "ms")
+        except socket.timeout:
+            print("REQUEST time out")
--- socket/udp_server.py
+++ socket/udp_server.py
@@ -1,1 +1,41 @@
-)
+import socket
+import gc
+
+HOST = "localhost"
+PORT = 1443
+g_socket = None
+
+def init():
+    try:
+        soket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print("Socket created!")
+    except socket.error as msg:
+        print("Failed to create socket ", msg[0], " msg ", msg[1] )
+        exit(-1)
+
+    try:
+        soket.bind((HOST, PORT))
+    except socket.error as msg:
+        print("bind failed ", msg[0], " msg ", msg[1])
+        exit(-1)
+
+    print("socket ok!")
+    return soket
+
+def tick():
+    while True:
+        data = g_socket.recvfrom(2048)
+        recievedData = data[0]
+        recievedAddr = data[1]
+
+        if(not data):
+            break
+        reply = "Received data " + data
+
+        g_socket.sendto(reply, recievedAddr)
+        print("IP addr: ", recievedAddr, " message ", receivedData)
+
+    g_socket.close()
+
+g_socket = init()
+tick()
